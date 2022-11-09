67ccf0183311f4533c483ffe49502c721451908c
Anchen Li <anchen.li@pslm-qxy9gygyvj.lan>
2022-11-10 01:38:36 +1100

    completerustlings watch

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let pair: Vec<&str> = s.split(",").collect();
+        if pair.len() != 2 {
+            return Person::default();
+        }
+
+        let name = pair[0].to_string();
+        if name == "" {
+            return Person::default();
+        }
+        if let Ok(age) = pair[1].to_string().parse() {
+            Person { name, age }
+        } else {
+            Person::default()
+        }
     }
 }
 
